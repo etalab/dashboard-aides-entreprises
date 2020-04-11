7ff743efe20a0acf17623c450f9abda31297b890
Geoffrey Aldebert <geoffrey.aldebert@gmail.com>
2020-04-11 16:47:36 +0200

    Correct sql insertion of naf table

--- scripts/sql/create_table_naf.sql
+++ scripts/sql/create_table_naf.sql
@@ -2,9 +2,7 @@ CREATE TABLE naf
 (
 	id_naf SERIAL PRIMARY KEY NOT NULL,
     code_naf CHARACTER VARYING,
-    intitule_naf CHARACTER VARYING,
-    intitule_naf_65 CHARACTER VARYING,
-    intitule_naf_40 CHARACTER VARYING
+    intitule_naf CHARACTER VARYING
 )
 TABLESPACE pg_default;
 
