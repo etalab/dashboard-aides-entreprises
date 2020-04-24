741387c0079c283aa902ee7f2ad6d55c5ae0ea23
Alexandre Bulté <alexandre@bulte.net>
2020-04-24 16:46:59 +0200

    add router support

--- frontend/plugins/matomo.js
+++ frontend/plugins/matomo.js
@@ -1,7 +1,10 @@
 import Vue from "vue"
 import VueMatomo from "vue-matomo"
 
-Vue.use(VueMatomo, {
-  host: "https://stats.data.gouv.fr",
-  siteId: 127,
-})
+export default ({ app }) => {
+  Vue.use(VueMatomo, {
+    host: "https://stats.data.gouv.fr",
+    siteId: 127,
+    router: app.router,
+  })
+}
