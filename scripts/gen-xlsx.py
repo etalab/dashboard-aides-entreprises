cf6376dc513bf1f51e8d1b55c07966fcd2fd9c8d
Geoffrey Aldebert <geoffrey.aldebert@gmail.com>
2020-05-18 18:02:49 +0200

    [Fonds de solidarite] Add classe effectif to published data

--- scripts/gen-xlsx.py
+++ scripts/gen-xlsx.py
@@ -12,3 +12,10 @@ read_file.to_excel ('../published-data/fonds-solidarite-volet-1-regional-categor
 
 read_file = pd.read_csv ("../published-data/fonds-solidarite-volet-1-departemental-categorie-juridique-latest.csv",dtype={'dispositif':str,'volet':str,'nombre_aides':float,'montant_total':float,'reg':str,'libelle_region':str,'dep':str,'libelle_departement':str,'code_categorie_juridique':str,'libelle_categorie_juridique':str})
 read_file.to_excel ('../published-data/fonds-solidarite-volet-1-departemental-categorie-juridique-latest.xlsx', index = None, header=True)
+
+
+read_file = pd.read_csv ("../published-data/fonds-solidarite-volet-1-regional-classe-effectif-latest.csv",dtype={'dispositif':str,'volet':str,'nombre_aides':float,'montant_total':float,'reg':str,'libelle_region':str,'classe_effectif':str,'libelle_classe_effectif':str})
+read_file.to_excel ('../published-data/fonds-solidarite-volet-1-regional-classe-effectif-latest.xlsx', index = None, header=True)
+
+read_file = pd.read_csv ("../published-data/fonds-solidarite-volet-1-departemental-classe-effectif-latest.csv",dtype={'dispositif':str,'volet':str,'nombre_aides':float,'montant_total':float,'reg':str,'libelle_region':str,'dep':str,'libelle_departement':str,'classe_effectif':str,'libelle_classe_effectif':str})
+read_file.to_excel ('../published-data/fonds-solidarite-volet-1-departemental-classe-effectif-latest.xlsx', index = None, header=True)
