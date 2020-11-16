6a38bc731f14410f466a8a3501e1edd80f163e55
Geoffrey Aldebert (bot) <geoffrey.aldebert@data.gouv.fr>
2020-11-16 14:25:10 +0100

    Reajust old data

--- scripts/monitoring/extract-input-info.py
+++ scripts/monitoring/extract-input-info.py
@@ -4,33 +4,38 @@ filenames = glob.glob('../../data/aides/aides-2020-*.csv')
 
 arr = []
 for filename in filenames:
-    mydict = {}
     print(filename.split("../../data/aides/aides-")[1].split(".csv")[0])
     df = pd.read_csv(filename)
-    df['Montant'] = df['Montant'].astype(float)
-    mydict['date_paiement'] = filename.split("../../data/aides/aides-")[1].split(".csv")[0]
-    mydict['nombre'] = df.shape[0]
-    mydict['montant'] = df['Montant'].sum()
-    arr.append(mydict)
-
-print(arr)
+    for dt in df.date_paiement.unique():
+        mydict = {}
+        df['Montant'] = df['Montant'].astype(float)
+        mydict['date_paiement'] = dt
+        mydict['nombre'] = df[df['date_paiement'] == dt].shape[0]
+        mydict['montant'] = df[df['date_paiement'] == dt]['Montant'].sum()
+        mydict['file'] = filename.split("../../data/aides/aides-")[1]
+        arr.append(mydict)
 
 df = pd.DataFrame(arr)
 
+df = df.groupby(['date_paiement','file'],as_index=False).sum()
+
+df.to_csv("../../data/stats/stats-files.csv",index=False)
+
+
 df2 = pd.read_csv('../../data/stats/extract-stats.csv')
 
-df3 = pd.merge(df,df2,on='date_paiement',how='left')
+df3 = pd.merge(df,df2,on='date_paiement',how='outer')
 
 df3.sort_values(by=['date_paiement'])
 
 arr = []
 for index,row in df3.iterrows():
     mydict = row
-    if(row['nombre_x'] == row['nombre_y']):
+    if(round(row['nombre_x'],0) == round(row['nombre_y'],0)):
         mydict['isNombreGood'] = True
     else:
         mydict['isSumGood'] = False
-    if(row['montant_x'] == row['montant_y']):
+    if(round(row['montant_x'],0) == round(row['montant_y'],0)):
         mydict['isSumGood'] = True
     else:
         mydict['isSumGood'] = False
@@ -38,5 +43,10 @@ for index,row in df3.iterrows():
 
 df4 = pd.DataFrame(arr)
 
+df4 = df4.sort_values(by=['date_paiement'])
+
 df4.to_csv("../../data/stats/stats-agg.csv",index=False)
 
+df5 = df4[(df4['isNombreGood'] == False) | (df4['isSumGood'] == False)]
+
+df5.to_csv("../../data/stats/stats-agg-errors.csv",index=False)
